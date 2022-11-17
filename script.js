75ea0d289b4e5c8ba586b4ebc761a663a28b2b48
VladislavaKosyrikhina <vladislava.kosyrikhina@gmail.com>
2022-11-17 20:21:32 +0300

    last commit

--- script.js
+++ script.js
@@ -1,14 +1,68 @@
-var number = + prompt ('Сколько фильмов вы смотрели?', '');
+let numberOfFilms;
 
-if (number == null && number.length >= 50){
-    return;
+function start () {
+    numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?');
+    }
+
 }
-if (number <= 10){
-    prompt ('Просмотрено довольно мало фильмов');
+
+start ();
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+function rememberMyFilms () {
+    for ( let i = 0; i < 2; i++) {
+        var a = prompt ('какой последний фильм вы посмотрели?', '');
+        var b = +prompt ('на сколько вы его оцениваете?', '');
+    
+        if ( a != null && b != null && a != '' && b != '' && a.length < 50 ) {
+        personalMovieDB.movies [a] = b;
+        console.log ('done');
+        } else {
+        console.log ('error');
+        i--;
+        }
+    }
 }
-if (number > 10 && number < 30){
-    prompt ('Вы классический кинозритель');
+
+rememberMyFilms ();
+
+function detectPersonalLevel () {
+    if (personalMovieDB.count < 10) {
+        console.log ('Вы посмотрели мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log ('Вы типичный киноман');
+    } else {
+        console.log ('Вы большой киноман');
+    }
 }
-if (number >= 30){
-    prompt ('Вы киноман');
+
+detectPersonalLevel ();
+
+function showByDB (){
+    if (personalMovieDB.privat == false) {
+        console.log (personalMovieDB);
+    } else {
+        console.log ('Error');
+    }
 }
+
+showByDB ();
+
+function writeYourGenres () {
+    for (i = 1; i < 4; i++) {
+        personalMovieDB.genres [i-1] = prompt (`Ваш любимый жанр ${i}`);
+    }
+}
+
+writeYourGenres ();
+console.log (personalMovieDB);
